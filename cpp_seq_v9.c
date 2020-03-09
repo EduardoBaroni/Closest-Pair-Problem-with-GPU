a77beef2992c6da5f8b6208068070ec0cf1e9355
medina325 <56964034+medina325@users.noreply.github.com>
2020-03-09 09:08:02 -0400

    Voltando

--- cpp_seq_v9.c
+++ cpp_seq_v9.c
@@ -142,7 +142,7 @@ int main(int argc, char *argv[])
 	float tempoTotal = (fim - inicio) / (float) CLOCKS_PER_SEC;
 	
 	#if DEBUG
-		printf("%.5f   :  %.5f      :     %.5f      :     %.5f    :  %.5f   :   %lf   :  %lf\n", 
+		printf("%.5f      %.5f            %.5f            %.5f       %.5f       %lf      %lf\n", 
 			leituraTempo, ordenacaoTempo, distanciasTempo, forcaBrutaTempo, tempoTotal, delta_inicial, delta_minimo);
 	#else
 		printf("   %.5f\n", tempoTotal);
